97a071f2c87a0a7b2787fb903582c236fdb32c7c
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-27 15:37:01 +0530

    ADD: Max working hrs and Max working days for a month added

--- EmployeeWageComputationProgram.java
+++ EmployeeWageComputationProgram.java
@@ -3,13 +3,14 @@ public class EmployeeWageComputationProgram
 {
 	public static void main(String[] args)
 	{
-//CONSTANTS
+		//CONSTANTS
 		final int IS_PRESENT = 1;
 		final int IS_FULL_TIME = 1;
 		final int WAGE_PER_HR = 20;
 		final int FULL_TIME_WORKING_HRS = 8;
 		final int PART_TIME_WORKING_HRS = 4;
 		final int WORKING_DAYS_PER_MONTH = 20;
+		final int MAX_WORKING_HRS = 100;
 
 		//VARIABLES
 		int checkPresence;
@@ -17,8 +18,9 @@ public class EmployeeWageComputationProgram
 		int empWage = 0;
 		int dayOfMonth = 0;
 		int totalWage = 0;
+		int workingHrs = 0;
 
-		while (dayOfMonth <= WORKING_DAYS_PER_MONTH)
+		while (dayOfMonth <= WORKING_DAYS_PER_MONTH && workingHrs <= MAX_WORKING_HRS)
 		{
 			checkPresence = (int) (Math.random()*10)%2;
 			checkEmpType = (int) (Math.random()*10)%2;
@@ -28,18 +30,21 @@ public class EmployeeWageComputationProgram
 				case IS_PRESENT:
 					switch (checkEmpType)
 					{
-						case IS_FULL_TIME:
-							empWage = WAGE_PER_HR * FULL_TIME_WORKING_HRS;
-							break;
-						default:
-							empWage = WAGE_PER_HR * PART_TIME_WORKING_HRS;
+					case IS_FULL_TIME:
+						empWage = WAGE_PER_HR * FULL_TIME_WORKING_HRS;
+						workingHrs += FULL_TIME_WORKING_HRS;
+						break;
+					default:
+						empWage = WAGE_PER_HR * PART_TIME_WORKING_HRS;
+						workingHrs += PART_TIME_WORKING_HRS;
 					}
 					break;
 				default:
-				empWage= 0;
+				empWage = 0;
+				workingHrs += 0;
 			}
 			totalWage += empWage;
 		}
-		System.out.println("Employee Monthly Wage: "+totalWage);
+		System.out.println("Employee Wage: "+totalWage);
 	}
 }
