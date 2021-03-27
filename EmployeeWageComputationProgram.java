27f656640dee8ed0a51cd1612d9c5564483d82a1
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-27 20:25:04 +0530

    ADD: Stored the employee total wages in the array

--- EmployeeWageComputationProgram.java
+++ EmployeeWageComputationProgram.java
@@ -1,21 +1,25 @@
 class WageCalculation
 {
 	//CONSTANTS
-	static final int IS_PRESENT = 1;
-	static final int IS_FULL_TIME = 1;
+	public static final int IS_PRESENT = 1;
+	public static final int IS_FULL_TIME = 1;
 
-	//method to store company details
-	public static void company()
+	//VARIABLES
+	private final String company;
+	private final int wage_per_hr;
+	private final int working_days_per_month;
+	private final int max_working_hrs;
+
+	public WageCalculation(String company, int wage_per_hr, int working_days_per_month,int max_working_hrs)
 	{
-		wageCalculator ("company1", 20, 20, 100);
-		wageCalculator ("company2", 10, 30, 110);
-		wageCalculator ("company3", 15, 10, 120);
-		wageCalculator ("company4", 30, 25, 130);
-		wageCalculator ("company5", 50, 15, 140);
+		this.company = company;
+		this.wage_per_hr = wage_per_hr;
+		this.working_days_per_month = working_days_per_month;
+		this.max_working_hrs = max_working_hrs;
 	}
 
 	//WAGE CALCULATOR
-	public static void wageCalculator(String company, int wage_per_hr, int working_days_per_month, int max_working_hrs)
+	public int wageCalculator()
 	{
 		//VARIABLES
 		int checkPresence;
@@ -53,13 +57,13 @@ class WageCalculation
 			 * empDailyWage = wage_per_hr * workingHrs;
 			 * System.out.println("Daily wage of employee: "+empDailyWage);
 			 */
-			totalWage = (wage_per_hr * totalWorkedHrs);
+			totalWage = (wage_per_hr * totalWorkedHrs); 
 			/* //can show wage until the current day
 			 * if(dayOfMonth <= working_days_per_month)
 			 * System.out.println("till the day "+dayOfMonth+" : "+totalWage);
 			 */
 		}
-		System.out.println("Employee Monthly Wage: "+totalWage);
+		return totalWage;
 	}
 }
 
@@ -68,7 +72,32 @@ public class EmployeeWageComputationProgram
 {
 	public static void main(String[] args)
 	{
-		//calling static method
-		WageCalculation.company();
+		WageCalculation wc1 = new WageCalculation("company1", 20, 20, 100);
+		WageCalculation wc2 = new WageCalculation("company2", 10, 30, 110);
+		WageCalculation wc3 = new WageCalculation("company3", 15, 10, 120);
+		WageCalculation wc4 = new WageCalculation("company4", 30, 25, 130);
+		WageCalculation wc5 = new WageCalculation("company5", 50, 15, 140);
+
+		//calling instance methods and storing wages of employees in the array	
+		int[] employeeWages = new int[5]; //declaring array
+
+		employeeWages[0] = wc1.wageCalculator();
+		employeeWages[1] = wc2.wageCalculator();
+		employeeWages[2] = wc3.wageCalculator();
+		employeeWages[3] = wc4.wageCalculator();
+		employeeWages[4] = wc5.wageCalculator();
+
+		/*//tried to use loop for calling method (FAILED)
+		 * for (int i=0;i<employeeWages.length;i++)
+		 * {
+		 * 		employeeWages[i] = wc(i+1).wageCalculator();
+		 * }
+		 */
+
+		//printing employee wages stored in the array
+		for (int i=0;i<employeeWages.length;i++)
+		{
+			System.out.println("Employee monthly wage of company_"+(i+1)+": "+employeeWages[i]);
+		}
 	}
 }
