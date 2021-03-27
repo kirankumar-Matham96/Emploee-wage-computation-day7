7cc3060b05e91e275dd950e327fdddf37a9d568b
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-27 13:56:48 +0530

    ADD: Checking Employee Presence

--- EmployeeWageComputationProgram.java
+++ EmployeeWageComputationProgram.java
@@ -1,6 +1,21 @@
 //main class
-public class EmployeeWageComputationProgram {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program!");
+public class EmployeeWageComputationProgram
+{
+	public static void main(String[] args)
+	{
+		//CONSTANTS
+		final int IS_PRESENT = 1;
+
+		//CHECKING EMPLOYEE PRESENCE
+		int checkPresence = (int) (Math.random()*10)%2;
+		if(checkPresence == IS_PRESENT)
+		{
+			System.out.println("Present");
+		}
+		else
+		{
+			System.out.println("Absent!");
+		}
+
 	}
 }
