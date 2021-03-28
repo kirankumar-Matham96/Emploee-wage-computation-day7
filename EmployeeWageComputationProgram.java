bc17338d5059b362a9efe8d6025aabb530937be5
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-28 23:36:16 +0530

    REFRACTED: Interface approach for managing multiple companies

--- EmployeeWageComputationProgram.java
+++ EmployeeWageComputationProgram.java
@@ -1,111 +1,129 @@
-import java.util.ArrayList;
-
-class WageCalculation
+//interface
+interface EmployeeWage
 {
-	// CONSTANTS
-	public final int IS_PRESENT = 1;
-	public final int IS_FULL_TIME = 1;
+	public void company(int wage_per_hr, int working_days_per_month, int max_working_hrs);
+	public int wageCalculator(CompanyInfo1 cI);
+}
 
+class CompanyInfo1
+{
 	// VARIABLES
-	private final String company;
-	private final int wage_per_hr;
-	private final int working_days_per_month;
-	private final int max_working_hrs;
+	public final int wage_per_hr;
+	public final int working_days_per_month;
+	public final int max_working_hrs;
+	public int totalWage = 0;
 
-	public WageCalculation(String company, int wage_per_hr, int working_days_per_month, int max_working_hrs)
+	public CompanyInfo1(int wage_per_hr, int working_days_per_month, int max_working_hrs)
 	{
-		this.company = company;
 		this.wage_per_hr = wage_per_hr;
 		this.working_days_per_month = working_days_per_month;
 		this.max_working_hrs = max_working_hrs;
 	}
 
-	//method to store company details
-	/*
-	* Return type ArrayList<Integer> is only for running in the terminal.
-	* If the ArrayList is not specified with the TYPE, then it will give the error mesage: 'Unverified conversion'.
-	* Then we either recompile with '-Xlint:unchecked' command or specify everything properly.
-	*/
-	public static ArrayList<Integer> company()	/* Made this method static so that when the method called, we don't need to pass parameters*/
+	public void setTotalWage(int totalWage)
 	{
-		ArrayList<Integer> totalWageofDiffEmp = new ArrayList<Integer>();
-
-		WageCalculation wc1 = new WageCalculation("company1", 20, 20, 100);
-		WageCalculation wc2 = new WageCalculation("company2", 10, 30, 110);
-		WageCalculation wc3 = new WageCalculation("company3", 15, 10, 120);
-		WageCalculation wc4 = new WageCalculation("company4", 30, 25, 130);
-		WageCalculation wc5 = new WageCalculation("company5", 50, 15, 140);
-
-		totalWageofDiffEmp.add(wc1.wageCalculator());
-		totalWageofDiffEmp.add(wc2.wageCalculator());
-		totalWageofDiffEmp.add(wc3.wageCalculator());
-		totalWageofDiffEmp.add(wc4.wageCalculator());
-		totalWageofDiffEmp.add(wc5.wageCalculator());
-
-		return totalWageofDiffEmp;
+		this.totalWage = totalWage;
 	}
 
-	// WAGE CALCULATOR
-	public int wageCalculator()
+	public String toString()
 	{
-		// VARIABLES
-		int checkPresence;
-		int checkEmpType;
-		// int empDailyWage = 0; //Used if uncomment the commented code-lines
-		int dayOfMonth = 0;
-		int totalWage = 0;
-		int workingHrs = 0;
-		int totalWorkedHrs = 0;
-
-		// Wage calculation
-		while (dayOfMonth <= working_days_per_month && totalWorkedHrs <= max_working_hrs) {
-			checkPresence = (int) (Math.random() * 10) % 2; // Presence check
-			checkEmpType = (int) (Math.random() * 10) % 2; // Work Type check
-			dayOfMonth++;
-			switch (checkPresence) {
-			case IS_PRESENT:
-				switch (checkEmpType) {
-				case IS_FULL_TIME:
-					workingHrs = 8;
-					totalWorkedHrs += workingHrs;
-					break;
-				default:
-					workingHrs = 4;
-					totalWorkedHrs += workingHrs;
-				}
-				break;
-			default:
-				totalWorkedHrs += 0;
-			}
-			/*
-			 * //can show the daily wage
-			 * empDailyWage = wage_per_hr * workingHrs;
-			 * System.out.println("Daily wage of employee: "+empDailyWage);
-			 */
-			totalWage = (wage_per_hr * totalWorkedHrs);
-			/*
-			 * //can show wage until the current day 
-			 * if(dayOfMonth <= working_days_per_month)
-			 * System.out.println("till the day "+dayOfMonth+" : "+totalWage);
-			 */
-		}
-		return totalWage;
+		return "total wage is: "+totalWage;
 	}
 }
 
 //MAIN CLASS
-public class EmployeeWageComputationProgram
+public class EmployeeWageComputationProgram implements EmployeeWage
 {
+	// CONSTANTS
+	public final int IS_PRESENT = 1;
+	public final int IS_FULL_TIME = 1;
+
+	//array diclaration
+	CompanyInfo1[] totalWageofDiffEmp = new CompanyInfo1[1];
+
+	//Object for Company Info method
+	CompanyInfo1 wcc;
+
+		//method to store company employee wage details
+		/*
+		* Made this method static so that when the method called, we don't need to pass parameters
+		*/
+		public void company(int wage_per_hr, int working_days_per_month, int max_working_hrs)
+		{
+			//Adding details to the array list (array of type 'CompanyInfo1')
+			wcc = new CompanyInfo1(wage_per_hr,working_days_per_month,max_working_hrs);
+
+			totalWageofDiffEmp[0] = wcc;
+
+			CompanyInfo1 cI;
+
+			//calling calculator method for every company
+			for(int i=0;i<totalWageofDiffEmp.length;i++)
+			{
+				cI = totalWageofDiffEmp[0];
+
+				int totalWage = wageCalculator(cI);
+				wcc.setTotalWage(totalWage);
+
+				System.out.println("total wage: "+cI);
+			}
+		}
+
+		// WAGE CALCULATOR
+		public int wageCalculator(CompanyInfo1 cI)
+		{
+			// VARIABLES
+			int checkPresence;
+			int checkEmpType;
+			// int empDailyWage = 0; //Used if uncomment the commented code-lines
+			int dayOfMonth = 0;
+			int totalWage = 0;
+			int workingHrs = 0;
+			int totalWorkedHrs = 0;
+
+			// Wage calculation
+			while (dayOfMonth <= cI.working_days_per_month && totalWorkedHrs <= cI.max_working_hrs)
+			{
+				checkPresence = (int) (Math.random() * 10) % 2; // Presence check
+				checkEmpType = (int) (Math.random() * 10) % 2; // Work Type check
+				dayOfMonth++;
+				switch (checkPresence)
+				{
+					case IS_PRESENT:
+						switch (checkEmpType)
+						{
+							case IS_FULL_TIME:
+								workingHrs = 8;
+								totalWorkedHrs += workingHrs;
+								break;
+							default:
+								workingHrs = 4;
+								totalWorkedHrs += workingHrs;
+						}
+						break;
+					default:
+						totalWorkedHrs += 0;
+				}
+				/*
+				 * //can show the daily wage empDailyWage = wage_per_hr * workingHrs;
+				 * System.out.println("Daily wage of employee: "+empDailyWage);
+				 */
+				totalWage = (cI.wage_per_hr * totalWorkedHrs);
+				/*
+				 * //can show wage until the current day if(dayOfMonth <=
+				 * working_days_per_month)
+				 * System.out.println("till the day "+dayOfMonth+" : "+totalWage);
+				 */
+			}
+			return totalWage;
+		}
+
 	public static void main(String[] args)
 	{
-		// calling instance methods and storing wages of employees in the array list
-		ArrayList<Integer> employeeWages = new ArrayList<Integer>(); // declaring array
-		employeeWages = WageCalculation.company();
+		EmployeeWage company1 = new EmployeeWageComputationProgram();
+		EmployeeWage company2 = new EmployeeWageComputationProgram();
 
-		// printing employee wages stored in the array list
-		for(int i = 0; i < employeeWages.size(); i++)
-		{
-			System.out.println("Employee monthly wage of company_" + (i + 1) + ": " +employeeWages.get(i));
-		}
+		company1.company(20, 20, 100);
+		company2.company(25, 15, 150);
 	}
 }
